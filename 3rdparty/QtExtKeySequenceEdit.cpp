5d3409d55e6187c8f56139db245dee05534af1d9
amirb <amirbasuti@gmail.com>
2025-09-29 19:59:54 +0330

    Enable combined key sequences for hotkeys
    Modified QtExtKeySequenceEdit to properly capture and handle multi-key sequences.
    Updated the hotkey settings dialog to use the enhanced QtExtKeySequenceEdit widget.

--- 3rdparty/QtExtKeySequenceEdit.cpp
+++ 3rdparty/QtExtKeySequenceEdit.cpp
@@ -3,22 +3,81 @@
 #include <QKeyEvent>
 
 QtExtKeySequenceEdit::QtExtKeySequenceEdit(QWidget *parent)
-        : QKeySequenceEdit(parent) {
+    : QKeySequenceEdit(parent)
+    , m_num(0)
+{
 }
 
-QtExtKeySequenceEdit::~QtExtKeySequenceEdit() {
+QKeySequence QtExtKeySequenceEdit::keySequence() const
+{
+    return m_keySequence;
 }
 
-void QtExtKeySequenceEdit::keyPressEvent(QKeyEvent *pEvent) {
-    QKeySequenceEdit::keyPressEvent(pEvent);
+void QtExtKeySequenceEdit::setKeySequence(const QKeySequence &keySequence)
+{
+    m_keySequence = keySequence;
+    QKeySequenceEdit::setKeySequence(m_keySequence);
+}
 
-    QKeySequence keySeq = keySequence();
-    if (keySeq.count() <= 0) {
+void QtExtKeySequenceEdit::keyPressEvent(QKeyEvent *pEvent)
+{
+    pEvent->accept();
+
+    int key = pEvent->key();
+    if (key == Qt::Key_unknown ||
+        key == Qt::Key_Control ||
+        key == Qt::Key_Shift ||
+        key == Qt::Key_Alt ||
+        key == Qt::Key_Meta)
+    {
         return;
     }
-    auto key = keySeq[0].key();
-    if (key == Qt::Key_Backspace || key == Qt::Key_Delete) {
-        key = static_cast<Qt::Key>(0);
-        setKeySequence(key);
+
+    if (key == Qt::Key_Backspace || key == Qt::Key_Delete)
+    {
+        m_num = 0;
+        m_keySequence = QKeySequence();
+        QKeySequenceEdit::setKeySequence(m_keySequence);
+        return;
     }
+
+    int modifiers = pEvent->modifiers();
+    if (modifiers & Qt::ControlModifier)
+    {
+        key += Qt::CTRL;
+    }
+    if (modifiers & Qt::ShiftModifier)
+    {
+        key += Qt::SHIFT;
+    }
+    if (modifiers & Qt::AltModifier)
+    {
+        key += Qt::ALT;
+    }
+    if (modifiers & Qt::MetaModifier)
+    {
+        key += Qt::META;
+    }
+
+    if (m_num == 0)
+    {
+        m_keySequence = QKeySequence(key);
+    }
+    else
+    {
+        int key_count = m_keySequence.count();
+        if (key_count > 0)
+        {
+            QVector<int> keys;
+            for (int i = 0; i < key_count; ++i)
+            {
+                keys.append(m_keySequence[i]);
+            }
+            keys.append(key);
+            m_keySequence = QKeySequence(keys);
+        }
+    }
+
+    QKeySequenceEdit::setKeySequence(m_keySequence);
+    m_num++;
 }
--- 3rdparty/QtExtKeySequenceEdit.h
+++ 3rdparty/QtExtKeySequenceEdit.h
@@ -1,11 +1,21 @@
 #include <QKeySequenceEdit>
+#include <QKeySequence>
 
 class QtExtKeySequenceEdit : public QKeySequenceEdit {
+    Q_OBJECT
 public:
-    QtExtKeySequenceEdit(QWidget *parent);
-
+    explicit QtExtKeySequenceEdit(QWidget *parent = 0);
     ~QtExtKeySequenceEdit();
 
+    QKeySequence keySequence() const;
+
+public slots:
+    void setKeySequence(const QKeySequence &keySequence);
+
 protected:
-    virtual void keyPressEvent(QKeyEvent *pEvent);
+    void keyPressEvent(QKeyEvent *pEvent);
+
+private:
+    int m_num;
+    QKeySequence m_keySequence;
 };
